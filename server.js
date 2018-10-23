f83b5408c076a2138de434d1c8d2d8ca42b733ed
gowthamkannan <gkbandep@iu.edu>
2018-10-23 10:22:43 -0400

    Fixing bugs related to Tus server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const tus = require('tus-node-server');
+const express = require('express');
 const server = new tus.Server();
 server.datastore = new tus.FileStore({
     path: '/files'
 });
 
-const express = require('express');
 const app = express();
 const uploadApp = express();
 uploadApp.all('*', server.handle.bind(server));
@@ -12,4 +12,12 @@ app.use('/uploads', uploadApp);
 
 const host = '127.0.0.1';
 const port = 8000;
-app.listen(port, host);
+
+// app.all('/files/*',function(req,res){
+// 	server.handle(req,res);
+
+// });
+
+app.listen(port,()=>{
+console.log(`[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`);
+});
